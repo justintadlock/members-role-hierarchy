16e495351373e93c6108cf70183ff916b5b52fbd
Justin Tadlock <justin@justintadlock.com>
2017-02-03 16:23:54 -0600

    Add initial files and folders.

---
+++ members-role-hierarchy.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Plugin Name: Members - Role Hierarchy
+ * Plugin URI:  http://themehybrid.com/plugins/members
+ * Description: Role Hierarchy is an add-on plugin for Members for creating a hierarchical roles system. Its purpose is to make sure that users who can edit, create, delete, and promote users cannot do so to users above them in the hierarchy.
+ * Version:     1.0.0-dev-20170203
+ * Author:      Justin Tadlock
+ * Author URI:  http://justintadlock.com
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @package   MembersRoleHierarchy
+ * @version   1.0.0
+ * @author    Justin Tadlock <justin@justintadlock.com>
+ * @copyright Copyright (c) 2017, Justin Tadlock
+ * @link      http://themehybrid.com/plugins/members-role-hierarchy
+ * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+/**
+ * Singleton class for setting up the plugin.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+final class Members_Role_Hierarchy_Plugin {
+
+	/**
+	 * Plugin directory path.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var    string
+	 */
+	public $dir = '';
+
+	/**
+	 * Plugin directory URI.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @var    string
+	 */
+	public $uri = '';
+
+	/**
+	 * Returns the instance.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return object
+	 */
+	public static function get_instance() {
+
+		static $instance = null;
+
+		if ( is_null( $instance ) ) {
+			$instance = new self;
+			$instance->setup();
+			$instance->includes();
+			$instance->setup_actions();
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Constructor method.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @return void
+	 */
+	private function __construct() {}
+
+	/**
+	 * Magic method to output a string if trying to use the object as a string.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function __toString() {
+		return 'members-role-hierarchy';
+	}
+
+	/**
+	 * Sets up globals.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	private function setup() {
+
+		// Main plugin directory path and URI.
+		$this->dir = trailingslashit( plugin_dir_path( __FILE__ ) );
+		$this->uri = trailingslashit( plugin_dir_url(  __FILE__ ) );
+	}
+
+	/**
+	 * Loads files needed by the plugin.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	private function includes() {
+
+		// Load function files.
+		require_once( $this->dir . 'inc/functions-filters.php'  );
+		require_once( $this->dir . 'inc/functions-options.php'  );
+		require_once( $this->dir . 'inc/functions-position.php' );
+
+		// Load admin files.
+		if ( is_admin() ) {
+
+			require_once( $this->dir . 'admin/functions-settings.php' );
+			require_once( $this->dir . 'admin/class-edit-role.php' );
+			require_once( $this->dir . 'admin/class-manage-roles.php' );
+		}
+	}
+
+	/**
+	 * Sets up main plugin actions and filters.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	private function setup_actions() {
+
+		// Internationalize the text strings used.
+		add_action( 'plugins_loaded', array( $this, 'i18n' ), 2 );
+	}
+
+	/**
+	 * Loads the translation files.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function i18n() {
+
+		load_plugin_textdomain( 'members-role-hierarchy', false, trailingslashit( dirname( plugin_basename( __FILE__ ) ) ). 'languages' );
+	}
+}
+
+Members_Role_Hierarchy_Plugin::get_instance();
